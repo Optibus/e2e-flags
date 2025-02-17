4f5902ed13767330bb4fae26cd8aa8ad16b9e31c
Wisam112 <wisam.armaly@optibus.com>
2025-02-17 11:22:34 +0200

    Remove airtable from e2e

--- src/services/cron/cron-with-secret.ts
+++ src/services/cron/cron-with-secret.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { AftermathAirtableFlagsProvider } from "flags-provider/aftermath-airtable";
+import { AftermathFlagsProvider } from "flags-provider/aftermath";
 import { RedisStorage } from "storage-provider/redis";
 import { logger } from "utils/logger";
 import { cronTaskV2, cronTask } from "./cron-task";
@@ -7,14 +7,9 @@ import { cronTaskV2, cronTask } from "./cron-task";
 dotenv.config();
 
 const aftermathSecret = process.env.AFTERMATH_SECRET;
-const airtableSecret = process.env.AIRTABLE_TOKEN;
 
 if (!aftermathSecret) {
   throw new Error("AFTERMATH_SECRET is not set");
-}
-
-if (!airtableSecret) {
-  throw new Error("AIRTABLE_TOKEN is not set");
 }
 
 const redis = new RedisStorage();
@@ -22,15 +17,11 @@ const redis = new RedisStorage();
 export const runTaskWithSecret = async (
   secrets: {
     aftermathSecret: string;
-    airtableSecret: string;
   },
   exitWhenDone: boolean = true
 ) => {
   logger.log("starting cron job");
-  const flagProvider = new AftermathAirtableFlagsProvider(
-    secrets.aftermathSecret,
-    secrets.airtableSecret
-  );
+  const flagProvider = new AftermathFlagsProvider(secrets.aftermathSecret);
   await cronTask(flagProvider, redis);
   await cronTaskV2(flagProvider, redis);
   if (exitWhenDone) {
@@ -43,7 +34,6 @@ export const runJob = async (exitWhenDone: boolean = true) => {
   return runTaskWithSecret(
     {
       aftermathSecret,
-      airtableSecret,
     },
     exitWhenDone
   );
--- src/services/server/index.ts
+++ src/services/server/index.ts
@@ -1,4 +1,4 @@
-import { AftermathAirtableFlagsProvider } from "../../flags-provider/aftermath-airtable";
+import { AftermathFlagsProvider } from "../../flags-provider/aftermath";
 import { LocalStorage } from "../../storage-provider/local";
 import { RedisStorage } from "../../storage-provider/redis";
 import { logger } from "../../utils/logger";
@@ -8,22 +8,14 @@ const redisStorage = new RedisStorage();
 const localStorage = new LocalStorage();
 
 const aftermathSecret = process.env.AFTERMATH_SECRET;
-const airtableSecret = process.env.AIRTABLE_TOKEN;
 
 if (!aftermathSecret) {
   throw new Error("AFTERMATH_SECRET is not set");
 }
 
-if (!airtableSecret) {
-  throw new Error("AIRTABLE_TOKEN is not set");
-}
-
-const aftermathAirtableFlagsProvider = new AftermathAirtableFlagsProvider(
-  aftermathSecret,
-  airtableSecret
-);
+const aftermathFlagsProvider = new AftermathFlagsProvider(aftermathSecret);
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-startServer(redisStorage, localStorage, aftermathAirtableFlagsProvider, port);
+startServer(redisStorage, localStorage, aftermathFlagsProvider, port);
 
 process.on("SIGTERM", () => {
   logger.log("SIGTERM");
